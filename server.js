dbaeb0b2124f26109d3a31c7d8d09694f90b2482
Yuri Gonçalves Lima <yurilima1231@gmail.com>
2024-11-26 01:22:28 -0300

    configuração de API com integração a Gemini e deploy no gogole cloud

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import routes from "./src/routes/postsRoutes.js";
 
 const app = express();
+//servindo arquivos estáticos, tornando público
+app.use(express.static("uploads"));
 routes(app);
 
 app.listen(3000, () => {
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,10 +1,17 @@
 import express from "express";
 import multer from "multer";
+import cors from "cors";
 import {
   listarPosts,
   postarNovoPost,
   uploadImagem,
+  atualizarNovoPost,
 } from "../controllers/postsController.js";
+
+const corsOptions = {
+  origin: "http://localhost:8000",
+  optionsSuccessStatus: 200,
+};
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -20,10 +27,12 @@ const upload = multer({ dest: "./uploads", storage });
 
 const routes = (app) => {
   app.use(express.json());
+  app.use(cors(corsOptions));
 
   app.get("/posts/", listarPosts);
   app.post("/posts", postarNovoPost);
   app.post("/upload", upload.single("imagem"), uploadImagem);
+  app.put("/upload/:id", atualizarNovoPost);
 };
 
 export default routes;
